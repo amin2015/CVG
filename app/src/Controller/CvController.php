391686ab508c33443e1a2adbb3ee179e3c252475
kevin <kevin.renaud91@gmail.com>
2022-11-13 15:04:35 +0100

    wip

--- app/src/Controller/CvController.php
+++ app/src/Controller/CvController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Cv;
+use App\Form\Cv1Type;
 use App\Form\CvType;
+use App\Repository\CvRepository;
 use App\Service\CvService;
 use App\Service\SkillService;
 use App\Service\SubthemeService;
@@ -45,6 +47,24 @@ class CvController extends AbstractController
         return $this->render('cv/new.html.twig', [
             'controller_name' => 'CvCon_newtroller',
             'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_cv_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Cv $cv, CvRepository $cvRepository): Response
+    {
+        $form = $this->createForm(CvType::class, $cv);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cvRepository->add($cv, true);
+
+            return $this->redirectToRoute('app_cv_controller2_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('cv/new.html.twig', [
+            'cv' => $cv,
+            'form' => $form
         ]);
     }
 
